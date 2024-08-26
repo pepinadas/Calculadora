efd8bdd437516c3c38d7721ee7a7f8603441a12b
dan1ela <danilalalaw@gmail.com>
2024-08-26 15:34:45 -0600

    Actualizando principal en la carpeta operaciones

--- Inicio/src/com/curso/v0/operaciones/Principal.java
+++ Inicio/src/com/curso/v0/operaciones/Principal.java
@@ -1,12 +1,48 @@
 package com.curso.v0.operaciones;
 
+import java.util.Scanner;
+
 import com.curso.v0.operaciones.Interface.Operacion;
 
 public class Principal {
 
     public static void main(String[] args) {
+        double num1, num2;
+        if(args != null){
+            if( args.length == 2){
+                try {
+                    num1 = Double.parseDouble(args[0]);
+                    num2 = Double.parseDouble(args[1]);    
+                } catch (Exception NumberFormatException) {
+                    System.out.println("El valor ingresado no es numerico");
+                }
+            }
+        }
+        else{
+
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.println("╔═════════════════════════════════════╗");
+            System.out.println("║      Bienvenid@ a la Calculadora    ║");
+            System.out.println("╚═════════════════════════════════════╝");
+    
+            System.out.print("Por favor, ingresa tu nombre: ");
+            String nombre = scanner.nextLine();
+    
+            // Saludar al usuario
+            System.out.println("\n¡Hola, " + nombre + "! Vamos a realizar algunas operaciones matemáticas.\n");
+    
+            // Solicitar el primer número
+            System.out.print("Por favor, ingresa el primer número: ");
+            num1 = scanner.nextDouble();
+    
+            // Solicitar el segundo número
+            System.out.print("Por favor, ingresa el segundo número: ");
+            num2 = scanner.nextDouble();
+        }
+
         Operacion operando = new Suma();
-        System.out.println(operando.ejecuta(1.1,2));
+        System.out.println(operando.ejecuta(num1, num2));
     }
 
 }
