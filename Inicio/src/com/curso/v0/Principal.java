a3ffde60d854419a32428fd1d0b1a9f2a5b59b65
OCADIZ <catalina.ocadiz@accenture.com>
2024-08-26 13:09:23 -0600

    Agregue la información general

--- Inicio/src/com/curso/v0/Principal.java
+++ Inicio/src/com/curso/v0/Principal.java
@@ -1,25 +1,31 @@
 package com.curso.v0;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("V1");
-			                 
-		Operacion[] operaciones  = {new Suma(8,4),
-									new Resta(8,4),
-									new Potencia(8,4),
-									new Multi(8,4),
-									new Division(8,4)};
-		show(operaciones);
-	}
-	
-	static void show(Operacion[] opes) {
-		//POLIMORFISMO
-		for(Operacion o:opes) {
-			System.out.println(o);
-			System.out.println(o.ejecuta());
-		}
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("╔═════════════════════════════════════╗");
+        System.out.println("║      Bienvenid@ a la Calculadora    ║");
+        System.out.println("╚═════════════════════════════════════╝");
+
+		System.out.print("Por favor, ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
+
+        // Saludar al usuario
+        System.out.println("\n¡Hola, " + nombre + "! Vamos a realizar algunas operaciones matemáticas.\n");
+
+        // Solicitar el primer número
+        System.out.print("Por favor, ingresa el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        // Solicitar el segundo número
+        System.out.print("Por favor, ingresa el segundo número: ");
+        double num2 = scanner.nextDouble();
+		
 	}
 
 }
