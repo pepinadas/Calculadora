434a7ccb713945e6ba4c088a3f8c77f6b1ed9755
OCADIZ <catalina.ocadiz@accenture.com>
2024-08-27 10:38:04 -0600

    Confirmación, no es valor numerico

--- Inicio/src/com/curso/v0/operaciones/Principal.java
+++ Inicio/src/com/curso/v0/operaciones/Principal.java
@@ -1,51 +1,40 @@
 package com.curso.v0.operaciones;
 
 import java.util.Scanner;
-
 import com.curso.v0.operaciones.Interface.Operacion;
-
-import java.util.Scanner;
-
-import static com.curso.v0.operaciones.Checked.isDouble;
 
 public class Principal {
 
     public static void main(String[] args) {
-        double num1, num2;
-        String opcion="";
+        double num1 = 0, num2 = 0;
+        String opcion = "";
         double resultado;
-        num1 = 0;
-        num2 = 0;
-
 
         if(args.length > 0){
-            if( args.length == 2){
+            if(args.length == 2){
                 try {
                     num1 = Double.parseDouble(args[0]);
                     num2 = Double.parseDouble(args[1]);    
-                } catch (Exception NumberFormatException) {
-                    System.out.println("El valor ingresado no es numerico");
+                } catch (NumberFormatException e) {
+                    System.out.println("El valor ingresado no es numérico");
+                    return;
                 }
             }
-        }
-        else{
-
+        } else {
             Scanner scanner = new Scanner(System.in);
-    
+
             System.out.println("╔═════════════════════════════════════╗");
             System.out.println("║      Bienvenid@ a la Calculadora    ║");
             System.out.println("╚═════════════════════════════════════╝");
-    
+
             System.out.print("Por favor, ingresa tu nombre: ");
             String nombre = scanner.nextLine();
-    
-            // Saludar al usuario
+
             System.out.println("\n¡Hola, " + nombre + "! Vamos a realizar algunas operaciones matemáticas.\n");
 
             Operacion operacion = null;
 
             do {
-                // Seleccionar el tipo de operacion
                 System.out.println("Selecciona la operación: +, -, *, /");
                 char oper = scanner.next().charAt(0);
                 scanner.nextLine();
@@ -53,12 +42,22 @@ public class Principal {
                 // Solicitar el primer número
                 System.out.print("Por favor, ingresa el primer número: ");
                 String tmp1 = scanner.nextLine();
-                num1 = Checked.isDouble(tmp1) ? Double.parseDouble(tmp1) : 0;
+                if (Checked.isDouble(tmp1)) {
+                    num1 = Double.parseDouble(tmp1);
+                } else {
+                    System.out.println("El valor ingresado no es numérico");
+                    continue;
+                }
 
                 // Solicitar el segundo número
                 System.out.print("\nPor favor, ingresa el segundo número: ");
                 String tmp2 = scanner.nextLine();
-                num2 = Checked.isDouble(tmp2) ? Double.parseDouble(tmp2) : 0;
+                if (Checked.isDouble(tmp2)) {
+                    num2 = Double.parseDouble(tmp2);
+                } else {
+                    System.out.println("El valor ingresado no es numérico");
+                    continue;
+                }
 
                 switch (oper) {
                     case '+':
@@ -75,6 +74,7 @@ public class Principal {
                         break;
                     default:
                         System.out.println("Operación no válida");
+                        continue;
                 }
 
                 if (operacion != null) {
@@ -86,12 +86,6 @@ public class Principal {
                 opcion = scanner.next();
 
             } while (opcion.equalsIgnoreCase("s"));
-
-
         }
-
-        Operacion operacion = new Suma();
-        System.out.println(operacion.ejecutar(num1, num2));
     }
-
 }
